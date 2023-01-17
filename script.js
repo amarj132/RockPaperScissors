8bee1745e4382e77683e18cb6c90d3aca8458e6e
amarj132 <amarjadhav49@gmail.com>
2023-01-17 21:27:55 +0530

    commit

---
+++ script.js
@@ -0,0 +1,89 @@
+
+const totalScore = { computerScore: 0, playerScore: 0 }
+
+function getComputerChoice() {
+  const rpsChoice = ['Rock', 'Paper', 'Scissors']
+  const randomNumber = Math.floor(Math.random() * 3)
+  return rpsChoice[randomNumber]
+}
+
+
+function getResult(playerChoice, computerChoice) {
+
+  let score;
+
+
+
+  if (playerChoice == computerChoice) {
+    score = 0
+  
+  } else if (playerChoice == 'Rock' && computerChoice == 'Scissors') {
+    score = 1
+  } else if (playerChoice == 'Paper' && computerChoice == 'Rock') {
+    score = 1
+  } else if (playerChoice == 'Scissors' && computerChoice == 'Paper') {
+    score = 1
+
+  } else {
+    score = -1
+  }
+
+  return score
+}
+
+function showResult(score, playerChoice, computerChoice) {
+  const resultDiv = document.getElementById('result')
+  const handDiv = document.getElementById('hands')
+  const playerScoreDiv = document.getElementById('player-score')
+
+  if (score == -1) {
+    resultDiv.innerText = 'You Lose!'
+  } else if (score == 0) {
+    resultDiv.innerText = 'Draw'
+  } else {
+    resultDiv.innerText = 'You Won!!'
+  }
+  handDiv.innerText = `${playerChoice} vs ${computerChoice}`
+  playerScoreDiv.innerText = `Your Score: ${totalScore['playerScore']}`
+ 
+}
+
+function onClickRPS(playerChoice) {
+  console.log({ playerChoice })
+  const computerChoice = getComputerChoice()
+  console.log({ computerChoice })
+  const score = getResult(playerChoice, computerChoice)
+  totalScore['playerScore'] += score
+
+  console.log({ score })
+  console.log(totalScore)
+  showResult(score, playerChoice, computerChoice)
+}
+
+
+function playGame() {
+  const rpsButtons = document.querySelectorAll('.rpsButton')
+
+ 
+  rpsButtons.forEach(rpsButton => {
+    rpsButton.onclick = () => onClickRPS(rpsButton.value)
+  })
+  const endGameButton = document.getElementById('endGameButton')
+  endGameButton.onclick = () => endGame(totalScore)
+
+
+}
+
+function endGame(totalScore) {
+  totalScore['playScore'] = 0
+  totalScore['computerScore'] = 0
+  const resultDiv = document.getElementById('result')
+  const handDiv = document.getElementById('hands')
+  const playerScoreDiv = document.getElementById('player-score')
+  resultDiv.innerText = ''
+  handDiv.innerText = ''
+  playerScoreDiv.innerText = ''
+
+}
+
+playGame()
